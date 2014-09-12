a716125186b8dd76d602a49309768de02616d5f8
Andreilys <andreilyskov@gmail.com>
2014-09-12 16:56:27 -0400

    Adding javascript+css
    new addition

---
+++ app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+
+            $(".scroll0").click(function (){
+                //$(this).animate(function(){
+                    $('html, body').animate({
+                        scrollTop: $(".intro").offset().top
+                    }, 500);
+                //});
+            });
+
+            $(".scroll1").click(function (){
+                //$(this).animate(function(){
+                    $('html, body').animate({
+                        scrollTop: $(".aboutMe").offset().top
+                    }, 500);
+                //});
+            });
+
+
+            $(".scroll2").click(function (){
+                //$(this).animate(function(){
+                    $('html, body').animate({
+                        scrollTop: $(".portfolio").offset().top
+                    }, 500);
+                //});
+            });
+
+
+            $(".scroll3").click(function (){
+                //$(this).animate(function(){
+                    $('html, body').animate({
+                        scrollTop: $(".contact").offset().top
+                    }, 500);
+                //});
+            });
+	
+
+
+	
+
+})
